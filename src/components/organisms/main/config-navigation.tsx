0a54b29cf3f034b0f77b2c53eed3a8180dd2ca47
truong-odinbi <truongnguyen@odinbi.com>
2024-12-03 16:43:48 +0700

    Fix UI

--- src/components/organisms/main/config-navigation.tsx
+++ src/components/organisms/main/config-navigation.tsx
@@ -5,17 +5,17 @@ import { Iconify } from '@/components/atoms/iconify';
 export const navConfig = [
   {
     title: 'Trang chủ',
-    icon: <Iconify icon="solar:home-2-bold-duotone" />,
+    icon: <Iconify icon="solar:home-2-linear" />,
     path: '/',
   },
   {
     title: 'Bảng xếp hạng rank',
-    icon: <Iconify icon="solar:atom-bold-duotone" />,
+    icon: <Iconify icon="ph:ranking-light" />,
     path: '/rank',
   },
   {
     title: 'Báo lỗi',
-    icon: <Iconify icon="solar:notebook-bold-duotone" />,
+    icon: <Iconify icon="octicon:report-24" />,
     path: 'https://www.facebook.com/diem.xuavanay.5/',
   },
 ];
